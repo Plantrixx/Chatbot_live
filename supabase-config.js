f9ce3c2bb72c91fe9c41c5addc14f1621a74e74f
Plantrixx <miguel@plantrixx.com>
2026-03-13 04:33:16 +0100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -6,4 +6,5 @@ const SUPABASE_URL = 'https://rcwxdkrzjxxdqnwquehd.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjd3hka3J6anh4ZHFud3F1ZWhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNjE0NzksImV4cCI6MjA4ODkzNzQ3OX0.y7sdAU5fwqfuiZdJZKPwxNou2CGN2rifvBHBE3eIO4U';
 
 // Init Supabase Client
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const { createClient } = window.supabase;
+window.supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
